c9fbcfc0a25aa06895d1af74928db58f95668031
지현 <spy03128@naver.com>
2021-09-19 22:16:52 +0900

    server에 동영상 저장하기

--- client/src/components/views/VideoUploadPage/VideoUploadePage.js
+++ client/src/components/views/VideoUploadPage/VideoUploadePage.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { Typography, Button, Form, message, Input, Icon } from "antd";
 import Dropzone from "react-dropzone";
+import Axios from "axios";
 
 const { Title } = Typography;
 const { TextArea } = Input;
@@ -39,6 +40,22 @@ function VideoUploadePage() {
     setCategory(e.currentTarget.value);
   };
 
+  const onDrop = (files) => {
+    let formData = new FormData();
+    const config = {
+      header: { "content-type": "multipart/form-data" },
+    };
+    formData.append("file", files[0]);
+
+    Axios.post("/api/video/uploadfiles", formData, config).then((response) => {
+      if (response.data.success) {
+        console.log(response.data);
+      } else {
+        alert("비디오 업로드를 실패했습니다.");
+      }
+    });
+  };
+
   return (
     <div style={{ maxWidth: "700px", margin: "2rem auto" }}>
       <div style={{ textAlign: "center", marginBottom: "2rem" }}>
@@ -48,7 +65,7 @@ function VideoUploadePage() {
       <Form onSubmit>
         <div style={{ display: "flex", justifyContent: "space-between" }}>
           {/* {Drop Zone} */}
-          <Dropzone onDrop multiple maxSize>
+          <Dropzone onDrop={onDrop} multiple={false} maxSize={1000000000}>
             {({ getRootProps, getInputProps }) => (
               <div
                 style={{
